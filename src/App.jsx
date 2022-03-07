339f9b13077955e2a241b2ed40fc73a7680cc170
pipi <a.mayora@live.com>
2022-03-06 23:49:34 -0300

    update of path components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useContext } from "react/cjs/react.development";
-import About from "./components/about/About";
-import Contact from "./components/contact/Contact";
-import Intro from "./components/intro/Intro";
-import ProyectList from "./components/proyectsList/ProyectList";
-import Toggle from "./components/toggle/Toggle";
-import ThemeContext from "./context/ThemeContext";
+import About from "./components/about/About.jsx";
+import Contact from "./components/contact/Contact.jsx";
+import Intro from "./components/intro/Intro.jsx";
+import ProyectList from "./components/proyectsList/ProyectList.jsx";
+import Toggle from "./components/toggle/Toggle.jsx";
+
+import ThemeContext from "./context/ThemeContext.js";
 
 const App = () => {
   const theme = useContext(ThemeContext);
