02de41aa7fd766281ba7c70f373484bdfcb3350f
pipi <a.mayora@live.com>
2022-03-07 19:04:51 -0300

    arreglo

--- src/data.js
+++ src/data.js
@@ -2,31 +2,31 @@ export const products = [
   {
     id: 1,
     img: "https://placeimg.com/300/400/arc",
-    link: "https://github.com/mindgames7/Portfolio",
+    link: "https://github.com/agustinmayorastrianese",
   },
   {
     id: 2,
     img: "https://placeimg.com/300/400/tech",
-    link: "https://github.com/mindgames7",
+    link: "https://github.com/agustinmayorastrianese",
   },
   {
     id: 3,
     img: "https://placeimg.com/300/400/animals",
-    link: "https://github.com/mindgames7",
+    link: "https://github.com/agustinmayorastrianese",
   },
   {
     id: 4,
     img: "https://placeimg.com/300/400/people",
-    link: "https://github.com/mindgames7",
+    link: "https://github.com/agustinmayorastrianese",
   },
   {
     id: 5,
     img: "https://placeimg.com/300/400/nature",
-    link: "https://github.com/mindgames7",
+    link: "https://github.com/agustinmayorastrianese",
   },
   {
     id: 6,
     img: "https://placeimg.com/300/400/sepia",
-    link: "https://github.com/mindgames7",
+    link: "https://github.com/agustinmayorastrianese",
   },
 ];
